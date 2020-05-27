f18c490b38da2a4f5c7c5a3862ff29cd75a12752
Joao Bertolino de Sousa Neto <joaobsneto@gmail.com>
2020-05-27 14:03:36 -0300

    -Adding more utils classes

---
+++ Runtime/Animator/AnimatorEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScriptUtils.AnimatorHelper {
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorEventHandler : MonoBehaviour {
+        private Animator animator;
+
+        private void Awake() {
+            animator = GetComponent<Animator>();
+        }
+
+        public void SetBoolToTrue(string parameterName) {
+            animator.SetBool(parameterName, true);
+        }
+
+        public void SetBoolToFalse(string parameterName) {
+            animator.SetBool(parameterName, false);
+        }
+
+        
+
+    }
+}
---
+++ Runtime/Transform/SetRotationFromEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace ScriptUtils.Events {
+    using UnityEngine;
+    //TODO: Rename this to SetRotationFromList and add lerp functionality
+    public class SetRotationFromEvent : MonoBehaviour {
+        [SerializeField]
+        private Quaternion[] m_rotations = null;
+
+        public void SetRotation(int index) {
+            transform.rotation = m_rotations[index];
+        }
+    }
+}
