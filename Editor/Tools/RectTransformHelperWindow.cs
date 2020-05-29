bb7a0386853a109410d5be7ec396e51218780161
Joao Bertolino de Sousa Neto <joaobsneto@gmail.com>
2020-05-29 19:32:02 -0300

    -Optimizing the StickAnchorsToBounds -Adding a function to change pivot to recttranform be on 0,0 anchoredposition

--- Editor/Tools/RectTransformHelperWindow.cs
+++ Editor/Tools/RectTransformHelperWindow.cs
@@ -1,6 +1,7 @@
 ﻿namespace Funbites.UnityUtils.Editor {
     using Sirenix.OdinInspector;
     using Sirenix.OdinInspector.Editor;
+    using System;
     using UnityEditor;
     using UnityEngine;
     public class RectTransformHelperWindow : OdinEditorWindow {
@@ -9,67 +10,37 @@
             GetWindow<RectTransformHelperWindow>().Show();
         }
 
+        private void ValidateTarget(RectTransform target)
+        {
+            if (target == null) throw new Exception("Set a target to perform operation.");
+            if (target.parent == null) throw new Exception("Target RectTransform must have a parent RectTransform.");
+        }
+
         [Button]
-        private void SetAnchorsToResetCoordinates(RectTransform target) {
-            if (target != null && target.parent != null && ShouldStick(target)) {
-                //Debug.Log("[Anchors Tools] Updating");
-                Stick(target);
-            }
+        private void StickAnchorsToBounds(RectTransform target) {
+            ValidateTarget(target);
+            Stick(target);
         }
 
-
-        static public Rect anchorRect;
-        static public Vector2 anchorVector;
-        static private Rect anchorRectOld;
-        static private Vector2 anchorVectorOld;
-        //static private RectTransform currentRectTransform;
-        //static private RectTransform parentRectTransform;
-        static private Vector2 pivotOld;
-        static private Vector2 offsetMinOld;
-        static private Vector2 offsetMaxOld;
-
-        static private bool ShouldStick(RectTransform currentRectTransform) {
-            return (
-                currentRectTransform.offsetMin != offsetMinOld ||
-                currentRectTransform.offsetMax != offsetMaxOld ||
-                currentRectTransform.pivot != pivotOld ||
-                anchorVector != anchorVectorOld ||
-                anchorRect != anchorRectOld
-                );
-        }
 
         static private void Stick(RectTransform currentRectTransform) {
-            CalculateCurrentWH(currentRectTransform);
-            CalculateCurrentXY(currentRectTransform);
-
-            CalculateCurrentXY(currentRectTransform);
-            pivotOld = currentRectTransform.pivot;
-            anchorVectorOld = anchorVector;
-
-            AnchorsToCorners(currentRectTransform);
-            anchorRectOld = anchorRect;
-
-            UnityEditor.EditorUtility.SetDirty(currentRectTransform.gameObject);
+            
+            Vector2 sizes = new Vector2(currentRectTransform.rect.width, currentRectTransform.rect.height);
+            Vector2 posXY = CalculateCurrentXY(currentRectTransform, sizes);
+            Rect anchorRect = new Rect(posXY, sizes);
+            AnchorsToCorners(currentRectTransform, anchorRect);
+            EditorUtility.SetDirty(currentRectTransform.gameObject);
         }
 
-        static private void CalculateCurrentXY(RectTransform currentRectTransform) {
+        static private Vector2 CalculateCurrentXY(RectTransform currentRectTransform, Vector2 sizes) {
             RectTransform parentRectTransform = currentRectTransform.parent.gameObject.GetComponent<RectTransform>();
-            float pivotX = anchorRect.width * currentRectTransform.pivot.x;
-            float pivotY = anchorRect.height * (1 - currentRectTransform.pivot.y);
-            Vector2 newXY = new Vector2(currentRectTransform.anchorMin.x * parentRectTransform.rect.width + currentRectTransform.offsetMin.x + pivotX - parentRectTransform.rect.width * anchorVector.x,
-                                      -(1 - currentRectTransform.anchorMax.y) * parentRectTransform.rect.height + currentRectTransform.offsetMax.y - pivotY + parentRectTransform.rect.height * (1 - anchorVector.y));
-            anchorRect.x = newXY.x;
-            anchorRect.y = newXY.y;
-            anchorRectOld = anchorRect;
+            float pivotX = sizes.x * currentRectTransform.pivot.x;
+            float pivotY = sizes.y * (1 - currentRectTransform.pivot.y);
+            return new Vector2(currentRectTransform.anchorMin.x * parentRectTransform.rect.width + currentRectTransform.offsetMin.x + pivotX,
+                                      -(1 - currentRectTransform.anchorMax.y) * parentRectTransform.rect.height + currentRectTransform.offsetMax.y - pivotY + parentRectTransform.rect.height);
         }
 
-        static private void CalculateCurrentWH(RectTransform currentRectTransform) {
-            anchorRect.width = currentRectTransform.rect.width;
-            anchorRect.height = currentRectTransform.rect.height;
-            anchorRectOld = anchorRect;
-        }
-
-        static private void AnchorsToCorners(RectTransform currentRectTransform) {
+        static private void AnchorsToCorners(RectTransform currentRectTransform, Rect anchorRect) {
             RectTransform parentRectTransform = currentRectTransform.parent.gameObject.GetComponent<RectTransform>();
 
             float pivotX = anchorRect.width * currentRectTransform.pivot.x;
@@ -78,15 +49,23 @@
             currentRectTransform.anchorMax = new Vector2(0f, 1f);
             currentRectTransform.offsetMin = new Vector2(anchorRect.x / currentRectTransform.localScale.x, anchorRect.y / currentRectTransform.localScale.y - anchorRect.height);
             currentRectTransform.offsetMax = new Vector2(anchorRect.x / currentRectTransform.localScale.x + anchorRect.width, anchorRect.y / currentRectTransform.localScale.y);
-            currentRectTransform.anchorMin = new Vector2(currentRectTransform.anchorMin.x + anchorVector.x + (currentRectTransform.offsetMin.x - pivotX) / parentRectTransform.rect.width * currentRectTransform.localScale.x,
-                                                     currentRectTransform.anchorMin.y - (1 - anchorVector.y) + (currentRectTransform.offsetMin.y + pivotY) / parentRectTransform.rect.height * currentRectTransform.localScale.y);
-            currentRectTransform.anchorMax = new Vector2(currentRectTransform.anchorMax.x + anchorVector.x + (currentRectTransform.offsetMax.x - pivotX) / parentRectTransform.rect.width * currentRectTransform.localScale.x,
-                                                     currentRectTransform.anchorMax.y - (1 - anchorVector.y) + (currentRectTransform.offsetMax.y + pivotY) / parentRectTransform.rect.height * currentRectTransform.localScale.y);
+            currentRectTransform.anchorMin = new Vector2(currentRectTransform.anchorMin.x + (currentRectTransform.offsetMin.x - pivotX) / parentRectTransform.rect.width * currentRectTransform.localScale.x,
+                                                     currentRectTransform.anchorMin.y - 1 + (currentRectTransform.offsetMin.y + pivotY) / parentRectTransform.rect.height * currentRectTransform.localScale.y);
+            currentRectTransform.anchorMax = new Vector2(currentRectTransform.anchorMax.x + (currentRectTransform.offsetMax.x - pivotX) / parentRectTransform.rect.width * currentRectTransform.localScale.x,
+                                                     currentRectTransform.anchorMax.y - 1 + (currentRectTransform.offsetMax.y + pivotY) / parentRectTransform.rect.height * currentRectTransform.localScale.y);
             currentRectTransform.offsetMin = new Vector2((0 - currentRectTransform.pivot.x) * anchorRect.width * (1 - currentRectTransform.localScale.x), (0 - currentRectTransform.pivot.y) * anchorRect.height * (1 - currentRectTransform.localScale.y));
             currentRectTransform.offsetMax = new Vector2((1 - currentRectTransform.pivot.x) * anchorRect.width * (1 - currentRectTransform.localScale.x), (1 - currentRectTransform.pivot.y) * anchorRect.height * (1 - currentRectTransform.localScale.y));
+        }
 
-            offsetMinOld = currentRectTransform.offsetMin;
-            offsetMaxOld = currentRectTransform.offsetMax;
+        [Button]
+        private void SetPivotToResetPosition(RectTransform target)
+        {
+            ValidateTarget(target);
+            if (target.anchorMin != target.anchorMax) throw new Exception("AnchorMin and AnchorMax must be the same for this operation");
+            if (target.localScale != Vector3.one) throw new Exception("This function does not work for scaled objects... it is good to TODO... sorry");
+            //TODO: implement scale support
+            target.pivot = -(target.anchoredPosition - target.rect.size * target.pivot) / target.rect.size;
+            target.anchoredPosition = Vector2.zero;
         }
     }
 }
