25eef863f19eca45e8d8dac6ded5b0f4f03685f8
Joao Bertolino de Sousa Neto <joaobsneto@gmail.com>
2020-06-02 11:52:24 -0300

    -Adding Util for Image to set native size on images that are horizontally squashed to fit better in atlas

---
+++ Editor/Tools/CanvasImageUtilityWindow.cs
@@ -0,0 +1,25 @@
+﻿namespace Funbites.UnityUtils.Editor
+{
+    using Sirenix.OdinInspector;
+    using Sirenix.OdinInspector.Editor;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class CanvasImageUtilityWindow : OdinEditorWindow
+    {
+        [MenuItem("Tools/Funbites/Canvas Image Utility")]
+        private static void OpenWindow()
+        {
+            GetWindow<CanvasImageUtilityWindow>().Show();
+        }
+        [Button]
+        private void SetNativeSizeWithAspectDeformation(Image image, float originalAspectWidth = 10, float originalAspectHeight = 16, float scale = .9f)
+        {
+            image.SetNativeSize();
+            int width = Mathf.CeilToInt(image.rectTransform.rect.width*scale);
+            int height = Mathf.CeilToInt(width / originalAspectWidth * originalAspectHeight);
+            image.rectTransform.sizeDelta = new Vector2(width, height);
+        }
+    }
+}
