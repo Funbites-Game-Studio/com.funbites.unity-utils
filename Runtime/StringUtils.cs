22c462cdcb54bf5cebb4c13aa7ad6bd28aefc804
Joao Bertolino de Sousa Neto <joaobsneto@gmail.com>
2020-09-20 20:35:39 -0300

    -Creating a StringUtils with a method to format time text

---
+++ Runtime/StringUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace Funbites.UnityUtils
+{
+    public static class StringUtils
+    {
+        public static string FormatMinutesToClockTime(this int minutes)
+        {
+            int hours = (minutes / 60);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendTwoDigitNumber(hours);
+            sb.Append(':');
+            minutes %= 60;
+            sb.AppendTwoDigitNumber(minutes);
+            return sb.ToString();
+        }
+
+        private static void AppendTwoDigitNumber(this StringBuilder sb, int number)
+        {
+            if (number > 10)
+            {
+                sb.Append(number);
+            } else
+            {
+                sb.Append('0');
+                sb.Append(number);
+            }
+        }
+    }
+}
